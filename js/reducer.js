aeae402dd6d42a866d2b419361adfaa37d44c213
Carla Holcomb <carla.programmer@gmail.com>
2021-01-16 21:26:24 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,11 +1,23 @@
 // add code snippets from README
-let state = {count: 0}
+let state;
 
-function reducer(state, action){
+function reducer(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return {count: state.count + 1};
+      return {count: state.count + 1}
     default:
       return state;
   }
 }
+
+function dispatch(action){
+  state = reducer(state, action)
+  render()
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({type: '@@INIT'})
